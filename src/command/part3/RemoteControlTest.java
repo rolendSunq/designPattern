4ff0dbf6838efc1e5d60c5d769c38b201a8cd6f8
rolendSunq <sunq0011@gmail.com>
2015-10-15 17:50:44 +0900

    Signed-off-by: rolendSunq <sunq0011@gmail.com>

--- src/command/part3/RemoteControlTest.java
+++ src/command/part3/RemoteControlTest.java
@@ -4,14 +4,13 @@ public class RemoteControlTest {
 	public static void main(String[] args) {
 		SimpleRemoteControl remote = new SimpleRemoteControl();
 		Light light = new Light();
-		GarageDoor garageDoor = new GarageDoor();
-		remote.setCommand(light.on());
+		LightOnCommand lightOnCommand = new LightOnCommand(light);
+		LightOffCommand lightOffCommand = new LightOffCommand(light);
+		remote.setCommand(lightOnCommand);
 		remote.buttonWasPressed();
-		remote.setCommand(garageDoor.up());
+		remote.setCommand(lightOffCommand);
 		remote.buttonWasPressed();
-		remote.setCommand(garageDoor.lightOn());
-		remote.buttonWasPressed();
-		remote.setCommand(garageDoor.lightOff());
+		
     }
 
 }
