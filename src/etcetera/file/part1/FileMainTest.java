c5b1bd47a48736279b5be0c9391bf4408866107e
rolendSunq <sunq0011@gmail.com>
2015-10-24 16:47:13 +0900

    Signed-off-by: rolendSunq <sunq0011@gmail.com>

---
+++ src/etcetera/file/part1/FileMainTest.java
@@ -0,0 +1,34 @@
+package etcetera.file.part1;
+
+import java.nio.file.attribute.*; 
+import java.io.*; 
+import java.util.*; 
+import java.nio.file.Path; 
+import java.nio.file.Paths; 
+import java.nio.file.StandardWatchEventKinds; 
+import java.nio.file.WatchEvent; 
+import java.nio.file.WatchKey; 
+import java.nio.file.WatchService; 
+import java.util.List; 
+
+public class FileMainTest {
+
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) { 
+		Path this_dir = Paths.get(".");
+		System.out.println("Now watching the current directory ...");
+		try { 
+			WatchService watcher = this_dir.getFileSystem().newWatchService();
+			this_dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE);
+			WatchKey watckKey = watcher.take();
+			List<WatchEvent<?>> events = watckKey.pollEvents();
+			for (WatchEvent event : events) { 
+				System.out.println("Someone just created the file '" + event.context().toString() + "'."); 
+			} 
+		} catch (Exception e) { 
+			System.out.println("Error: " + e.toString()); 
+		} 
+	} 
+} 
+
+
